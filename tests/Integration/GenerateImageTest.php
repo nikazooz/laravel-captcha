65deee58fd872d01f94fb9688e71c908b1793a93
Nenad Živanović <nikazooz@users.noreply.github.com>
2025-07-10 09:40:13 +0200

    Add support for Laravel 12 (#4)
    * Tweak how we check if test should be skipped
    
    * Instead of docblock annotation use method prefix to declare tests
    
    This makes them universally discoverable for supported phpunit versions.
    
    * Support Laravel 12 and PHPUnit 12
    
    * Update GH action to test with Laravel 12

--- tests/Integration/GenerateImageTest.php
+++ tests/Integration/GenerateImageTest.php
@@ -7,16 +7,15 @@ use Nikazooz\LaravelCaptcha\Tests\IntegrationTest;
 
 class GenerateImageTest extends IntegrationTest
 {
-    /** @test */
-    public function can_generate_captcha_image_when_requested_using_gd()
+    public function test_can_generate_captcha_image_when_requested_using_gd()
     {
-        $this->setConfig('driver', 'gd');
-
-        if (! Captcha::isAvailable()) {
+        if (! function_exists('gd_info')) {
             $this->markTestSkipped(
                 'The GD extension is not available.'
             );
         }
+
+        $this->setConfig('driver', 'gd');
 
         $response = $this->withoutExceptionHandling()->get(Captcha::url());
 
@@ -31,17 +30,16 @@ class GenerateImageTest extends IntegrationTest
         $this->assertEquals($expectedType, $type);
     }
 
-    /** @test */
-    public function can_generate_captcha_image_when_requested_using_imagick()
+    public function test_can_generate_captcha_image_when_requested_using_imagick()
     {
-        $this->setConfig('driver', 'imagick');
-
-        if (! Captcha::isAvailable()) {
+        if (! class_exists('Imagick')) {
             $this->markTestSkipped(
                 'The Imagick extension is not available.'
             );
         }
 
+        $this->setConfig('driver', 'imagick');
+
         $response = $this->withoutExceptionHandling()->get(Captcha::url());
 
         $response->assertStatus(200);
@@ -55,16 +53,15 @@ class GenerateImageTest extends IntegrationTest
         $this->assertEquals($expectedType, $type);
     }
 
-    /** @test */
-    public function can_configure_image_width_and_height()
+    public function test_can_configure_image_width_and_height()
     {
-        $this->setConfig('driver', 'gd');
-
-        if (! Captcha::isAvailable()) {
+        if (! function_exists('gd_info')) {
             $this->markTestSkipped(
                 'The GD extension is not available.'
             );
         }
+
+        $this->setConfig('driver', 'gd');
 
         $this->setConfig('image.width', 500);
         $this->setConfig('image.height', 100);
